91bd88f260e6c882bde8235b2bc27432c76b1d9a
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-12-07 20:05:00 +0200

    fix the bug

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import "dotenv/config";
-import { PrismaClient } from "./prisma/client.js";
+import { PrismaClient } from "./prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
